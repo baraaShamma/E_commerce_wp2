686115ec58d22ea98edc2699fb488a9f685f36f5
baraaShamma <baraa.shamma@gmail.com>
2024-10-31 01:52:25 +0300

    header

--- process_checkout.php
+++ process_checkout.php
@@ -10,24 +10,20 @@ if (!isset($_SESSION['user_id'])) {
 $user_id = $_SESSION['user_id'];
 $total_price = $_POST['total_price'];
 
-// إضافة الطلب الجديد إلى جدول orders
 $sql = "INSERT INTO orders (user_id, total_price, status) VALUES ($user_id, $total_price, 'قيد التجهيز')";
 $conn->query($sql);
 $order_id = $conn->insert_id;
 
-// جلب بيانات السلة مع السعر من جدول products
 $sql = "SELECT cart.product_id, cart.quantity, products.price 
         FROM cart 
         JOIN products ON cart.product_id = products.id 
         WHERE cart.user_id = $user_id";
 $result = $conn->query($sql);
 
-// التحقق من نجاح الاستعلام
 if ($result === false) {
     die("Error fetching cart data: " . $conn->error);
 }
 
-// إضافة المنتجات المطلوبة إلى جدول order_items
 while ($row = $result->fetch_assoc()) {
     $product_id = $row['product_id'];
     $quantity = $row['quantity'];
