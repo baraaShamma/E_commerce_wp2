eac436177de25d807102ff65b8ded418f9ee1669
baraaShamma <baraa.shamma@gmail.com>
2024-10-31 01:15:07 +0300

    db

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root";   
-$password = "";        
-$dbname = "onlineStore";
+$servername = getenv('MYSQLHOST') ?: 'localhost';
+$username = getenv('MYSQLUSER') ?: 'root';
+$password = getenv('MYSQLPASSWORD') ?: '';
+$dbname = getenv('MYSQLDATABASE') ?: 'onlineStore';
+$port = getenv('MYSQLPORT') ?: 3306;
 
 // إنشاء الاتصال
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
 // التحقق من الاتصال
 if ($conn->connect_error) {
